3ec96d15ef62f1074a1a9cae4834ac654b8e2202
SamuelStubb <98777575+SamuelStubb@users.noreply.github.com>
2022-12-05 21:30:54 -0500

    Update HowToActivity.java

--- app/src/main/java/com/example/memorygame/HowToActivity.java
+++ app/src/main/java/com/example/memorygame/HowToActivity.java
@@ -1,9 +1,12 @@
 package com.example.memorygame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.Objects;
 
@@ -13,6 +16,18 @@ public class HowToActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), HomeScreen.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+    }
+
 }
