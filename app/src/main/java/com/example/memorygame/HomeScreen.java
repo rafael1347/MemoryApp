db5c35aca39cebf4c21ab9f4aa48b3e17f6971c5
SamuelStubb <98777575+SamuelStubb@users.noreply.github.com>
2022-12-06 17:10:46 -0500

    Update HomeScreen.java

--- app/src/main/java/com/example/memorygame/HomeScreen.java
+++ app/src/main/java/com/example/memorygame/HomeScreen.java
@@ -19,13 +19,21 @@ public class HomeScreen extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+    
     public void onCustomizeClick(View view) {
         Intent intent=new Intent(HomeScreen.this, ColorActivity.class);
         startActivity(intent);
         finish();
     }
+    
     public void onHowtoPlayClick(View view) {
         Intent intent=new Intent(HomeScreen.this, HowToActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    
+        public void onHighscoreButtonClick(View view) {
+        Intent intent=new Intent(HomeScreen.this, HighScore.class);
         startActivity(intent);
         finish();
     }
